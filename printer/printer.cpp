df40313b2ffb9ee7d61d7f25299247b768a7298c
Corey Garvey <CGarv16@gmail.com>
2017-06-29 20:38:38 +0100

    print each digit as hex separately

--- printer/printer.cpp
+++ printer/printer.cpp
@@ -13,8 +13,26 @@ using namespace std;
 /* Function to print file from filename */
 bool print_file(const char* filename){
     cout << "Printing from file '" << filename << "'" << endl;
-    //ifstream in(filename);
+    
+    ifstream in_stream;
 
-
+    /* Open input file, convert to hex, output */
+    in_stream.open(filename);
+    
+    char character;
+    int digit;
+   
+    in_stream >> character;
+    //int count = 0;
+    while(!in_stream.eof()){
+        //count = count + 1;
+        //cout << " The count is: " << dec << count << endl;
+        cout << hex << int(character) << " ";
+        in_stream >> character;
+    }
+    
+    cout << endl;
+    in_stream.close();
+    //cout << "The count is: " << dec << count << endl;
     return true;
 }
