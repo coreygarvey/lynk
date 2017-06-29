11173137ee8e3f4575fd2699f6067b8b09050f56
Corey Garvey <cg916@ray10.doc.ic.ac.uk>
2017-06-29 19:21:34 +0100

    first commit of printer

---
+++ printer/errors.h
@@ -0,0 +1,8 @@
+/* Lynk - Printer Error Codes */
+
+/* Corey Garvey
+ * Last Edited 29 June 2017
+ */
+
+#define NO_FILENAME         1
+#define TOO_MANY_PARAMS     2
---
+++ printer/printer.cpp
@@ -0,0 +1,20 @@
+/* Lynk printer */
+
+/* Corey Garvey
+ * Last Updated June 29 2017
+ */
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include "printer.h"
+using namespace std;
+
+/* Function to print file from filename */
+bool print_file(const char* filename){
+    cout << "Printing from file '" << filename << "'" << endl;
+    //ifstream in(filename);
+
+
+    return true;
+}
