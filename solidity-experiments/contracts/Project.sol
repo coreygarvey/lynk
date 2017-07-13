f65e089f69e71a4a60e54e54b1e6ea4a57fd3717
Corey Garvey <CGarv16@gmail.com>
2017-07-13 14:23:28 +0100

    added versioning for tests, details, and template files

--- solidity-experiments/contracts/Project.sol
+++ solidity-experiments/contracts/Project.sol
@@ -8,11 +8,7 @@ contract Project {
   // state
   address public owner;
   
-  // Signatures of template, details, and tests files
-  string public template;
-  string public details;
-  string public tests;
-
+  
 
   // Version of file
   struct Version {
@@ -20,7 +16,77 @@ contract Project {
       string signature;
   }
 
-  // Mapping between user address and versions
+  // Core Project Files
+  //  projectFiles[0] = Templates
+  //  projectFiles[1] = Details
+  //  projectFiles[2] = Tests
+  mapping (uint => Version[]) public projectFiles;
+    
+
+  // projectFiles[0] corresponds to templates
+  function setTemplate(bytes32 hash, string templateSig) onlyOwner {
+      projectFiles[0].push(Version({
+          hash: hash,
+          signature: templateSig
+      }));
+      return;
+  }
+  
+  function getTemplate(string signature) constant returns (bytes32){
+      uint templateFileCount = projectFiles[0].length;
+      for (uint i=0; i<templateFileCount; i++){
+        var template = projectFiles[0][i];
+        if(sha3(template.signature) == sha3(signature)){
+            bytes32 hash = template.hash;
+            return hash;
+        }
+      }
+      return 0;
+  }
+
+  // projectFiles[1] corresponds to details
+  function setDetails(bytes32 hash, string detailsSig) onlyOwner {
+      projectFiles[1].push(Version({
+          hash: hash,
+          signature: detailsSig
+      }));
+      return;
+  }
+  
+  function getDetails(string signature) constant returns (bytes32){
+      uint detailsFileCount = projectFiles[1].length;
+      for (uint i=0; i<detailsFileCount; i++){
+        var details = projectFiles[1][i];
+        if(sha3(details.signature) == sha3(signature)){
+            bytes32 hash = details.hash;
+            return hash;
+        }
+      }
+      return 0;
+  }
+
+  // projectFiles[2] corresponds to tests
+  function setTests(bytes32 hash, string testsSig) onlyOwner {
+      projectFiles[2].push(Version({
+          hash: hash,
+          signature: testsSig
+      }));
+      return;
+  }
+  
+  function getTests(string signature) constant returns (bytes32){
+      uint testsFileCount = projectFiles[0].length;
+      for (uint i=0; i<testsFileCount; i++){
+        var tests = projectFiles[0][i];
+        if(sha3(tests.signature) == sha3(signature)){
+            bytes32 hash = tests.hash;
+            return hash;
+        }
+      }
+      return 0;
+  }
+
+  // Commited versions of project 
   mapping (address => Version[]) public versions;
 
   // User commiting a hash:signature pair to contract
@@ -40,7 +106,6 @@ contract Project {
 
   // retrieving the hash from the signature
   function returnHash(string signature) constant returns (bytes32) {
-
       uint length = versions[msg.sender].length;
       for (uint i=0; i<length; i++){
           var version = versions[msg.sender][i];
@@ -116,24 +181,5 @@ contract Project {
 
   */
  
-  function setTemplate(string templateSig) onlyOwner {
-      template = templateSig;
-  }
-  function getTemplate() constant returns (string){
-      return template;
-  }
 
-  function setDetails(string detailsSig) onlyOwner {
-      details = detailsSig;
-  }
-  function getDetails() constant returns (string){
-      return details;
-  }
-
-  function setTests(string testsSig) onlyOwner {
-      tests = testsSig;
-  }
-  function getTests() constant returns (string){
-      return tests;
-  }
 }
