a0bea4391528a407538b9582112eb72a77d270a6
Corey Garvey <CGarv16@gmail.com>
2017-07-13 13:18:44 +0100

    removed previous versions setup

--- solidity-experiments/contracts/Project.sol
+++ solidity-experiments/contracts/Project.sol
@@ -13,9 +13,6 @@ contract Project {
   string public details;
   string public tests;
 
-  // Mapping between version signatures (currently hashes)
-  //    and the pubKey that signed (user's eth address)
-  mapping (bytes32 => string) public versions;
 
   // Version of file
   struct Version {
@@ -24,10 +21,10 @@ contract Project {
   }
 
   // Mapping between user address and versions
-  mapping (address => Version[]) public versionLists;
+  mapping (address => Version[]) public versions;
 
   function commitVersion(bytes32 hash, string signature){
-      versionLists[msg.sender].push(Version({
+      versions[msg.sender].push(Version({
           hash: hash,
           signature: signature
       }));
@@ -35,9 +32,9 @@ contract Project {
   }
 
   function returnHash(string signature) constant returns (bytes32) {
-      uint length = versionLists[msg.sender].length;
+      uint length = versions[msg.sender].length;
       for (uint i=0; i<length; i++){
-          var version = versionLists[msg.sender][i];
+          var version = versions[msg.sender][i];
           if(sha3(version.signature) == sha3(signature)){
               bytes32 hash = version.hash;
               return hash;
@@ -110,27 +107,6 @@ contract Project {
 
   */
  
-  function bytes32ToString(bytes32 x) constant returns (string) {
-      bytes memory bytesString = new bytes(32);
-      uint charCount = 0;
-      for (uint j = 0; j < 32; j++) {
-          byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
-          if (char != 0) {
-              bytesString[charCount] = char;
-              charCount++;
-          }
-      }
-      bytes memory bytesStringTrimmed = new bytes(charCount);
-      for (j = 0; j < charCount; j++) {
-          bytesStringTrimmed[j] = bytesString[j];
-      }
-      return string(bytesStringTrimmed);
-  }
-
-
-
-
-
   function setTemplate(string templateSig){
       if(msg.sender != owner) return;
       template = templateSig;
@@ -153,12 +129,5 @@ contract Project {
   }
   function getTests() constant returns (string){
       return tests;
-  }
-  function setVersion(bytes32 versionSig, string sig){
-      versions[versionSig] = sig;
-  }
-  
-  function getVersion(bytes32 versionSig) constant returns (string){
-      return versions[versionSig];
   }
 }
